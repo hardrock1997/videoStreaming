d74ffbd5478a2e7054ba348ca9718ec4e42775aa
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2023-10-05 12:25:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.js}"
+    // './pages/**/*.{html,js}',
+    // './components/**/*.{html,js}',
+    ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
